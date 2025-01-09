ddc14ac763201ae8ac3a3c4babeb8f744c816e27
Spearking91 <baninacquah@gmail.com>
2025-01-09 09:29:06 +0000

    last change 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "../components/ProductCard";
 import NavBar from "../components/NavBar";
 import CustomButton from "../components/CustomButton";
 import Slide from "@/components/Slide";
@@ -24,80 +21,116 @@ export default function Home() {
             <CustomButton title={"Get Started"} click={undefined} />
           </div>
         </section>
-        <section className="bg-secondary h-auto w-full justify-center items-center p-10 text-center flex flex-row" id="c1">
-            <div className=" h-96 flex flex-row m-5">
-              <div className="flex-1 border-2 border-amber-300">
-              <img className="object-cover w-full h-full" src="/images/superCar(5).jpg" alt="" />
-              </div>
-              <div className="flex-1 border-2 border-blue-500">
-              <img className="object-cover w-full h-full" src="/images/superCar(8).jpg" alt="" />
-              </div>
-              <div className="flex-1 border-2 border-orange-400">
-                <img className="object-cover w-full h-full" src="/images/superCar(9).jpg" alt="" />
-              </div>
+        <section
+          className="bg-secondary h-auto w-full justify-center items-center p-10 text-center flex flex-row"
+          id="c1"
+        >
+          <div className=" h-96 flex flex-row m-5">
+            <div className="flex-1 border-2 border-amber-300">
+              <img
+                className="object-cover w-full h-full"
+                src="/images/superCar(5).jpg"
+                alt=""
+              />
             </div>
-            <div>
-              <h1 className="text-2xl text-primary">Home</h1>
+            <div className="flex-1 border-2 border-blue-500">
+              <img
+                className="object-cover w-full h-full"
+                src="/images/superCar(8).jpg"
+                alt=""
+              />
+            </div>
+            <div className="flex-1 border-2 border-orange-400">
+              <img
+                className="object-cover w-full h-full"
+                src="/images/superCar(9).jpg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div>
+            <h1 className="text-2xl text-primary">Home</h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
               ipsum porro assumenda laborum reprehenderit nulla doloribus
               debitis qui nemo optio fugiat voluptatum eveniet deleniti, aut
               magni, animi tempore excepturi dicta.
             </p>
-            </div>
+          </div>
         </section>
-        
+
         <section id="c2" className="flex-row-reverse flex w-full h-auto">
           <div className="w-1/2 ">
-          <Slide />
+            <Slide
+              time={6}
+              slides={[
+                { src: "/images/superCar(3).jpg" },
+                { src: "/images/superCar(4).jpg" },
+                { src: "/images/superCar(2).jpg" },
+              ]}
+            />
           </div>
           <div className="bg-secondary p-10 justify-center text-center items-center flex w-1/2 flex-col">
-          <div>
+            <div>
               <h1 className="text-2xl text-primary">Contact</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-              ipsum porro assumenda laborum reprehenderit nulla doloribus
-              debitis qui nemo optio fugiat voluptatum eveniet deleniti, aut
-              magni, animi tempore excepturi dicta.
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+                ipsum porro assumenda laborum reprehenderit nulla doloribus
+                debitis qui nemo optio fugiat voluptatum eveniet deleniti, aut
+                magni, animi tempore excepturi dicta.
+              </p>
             </div>
             <CustomButton title={"Click"} click={undefined} />
           </div>
         </section>
 
         <div className="bg-secondary p-10" />
-        
+
         <section id="c3" className="flex-row flex w-full h-auto ">
-        <div className="w-1/2 ">
-          <Slide />
+          <div className="w-1/2 ">
+            <Slide
+              time={5}
+              slides={[
+                { src: "/images/superCar(1).jpg" },
+                { src: "/images/superCar(8).jpg" },
+                { src: "/images/superCar(9).jpg" },
+              ]}
+            />
           </div>
           <div className="bg-secondary p-10 justify-center text-center items-center flex w-1/2 flex-col">
-          <div>
+            <div>
               <h1 className="text-2xl text-primary">About</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-              ipsum porro assumenda laborum reprehenderit nulla doloribus
-              debitis qui nemo optio fugiat voluptatum eveniet deleniti, aut
-              magni, animi tempore excepturi dicta.
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+                ipsum porro assumenda laborum reprehenderit nulla doloribus
+                debitis qui nemo optio fugiat voluptatum eveniet deleniti, aut
+                magni, animi tempore excepturi dicta.
+              </p>
             </div>
             <CustomButton title={"Click"} click={undefined} />
           </div>
         </section>
         <div className="bg-secondary p-10" />
         <section id="c4" className="flex-row-reverse flex w-full h-auto">
-        <div className="w-1/2 ">
-          <Slide />
+          <div className="w-1/2 ">
+            <Slide
+              time={3}
+              slides={[
+                { src: "/images/superCar(9).jpg" },
+                { src: "/images/superCar(2).jpg" },
+                { src: "/images/superCar(8).jpg" },
+              ]}
+            />
           </div>
           <div className="bg-secondary p-10 justify-center text-center items-center flex flex-1">
-          <div>
+            <div>
               <h1 className="text-2xl text-primary">Help</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-              ipsum porro assumenda laborum reprehenderit nulla doloribus
-              debitis qui nemo optio fugiat voluptatum eveniet deleniti, aut
-              magni, animi tempore excepturi dicta.
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+                ipsum porro assumenda laborum reprehenderit nulla doloribus
+                debitis qui nemo optio fugiat voluptatum eveniet deleniti, aut
+                magni, animi tempore excepturi dicta.
+              </p>
             </div>
           </div>
         </section>
--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,35 +1,35 @@
+"use client";
+import React, { useState, useEffect } from "react";
 
-
-
-'use client'
-import React, { useState, useEffect } from 'react';
-
-interface props{
-    src: any;
-    alt: string;
+interface props {
+  slides?: any;
+  time?: number;
 }
 
-const slides = [
-  { src: '/images/superCar(5).jpg', alt: 'Slide 1' },
-  { src: '/images/superCar(2).jpg', alt: 'Slide 2' },
-  { src: '/images/superCar(3).jpg', alt: 'Slide 3' },
-];
-
-function App() {
+function App({
+  time = 3,
+  slides = [
+    { src: "/images/superCar(5).jpg", alt: "Slide 1" },
+    { src: "/images/superCar(2).jpg", alt: "Slide 2" },
+    { src: "/images/superCar(3).jpg", alt: "Slide 3" },
+  ],
+}: props) {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-    }, 4000); // Change slide every 3 seconds
+    }, time * 1000); // Change slide every 3 seconds
 
     return () => clearInterval(intervalId);
   }, []);
 
-  
-
   return (
-<img className={`transition-opacity duration-1000 ease-linear object-cover w-full h-full`} src={slides[currentSlide].src} alt={slides[currentSlide].alt} />
+    <img
+      className={`transition-opacity duration-1000 ease-linear object-cover w-full h-full`}
+      src={slides[currentSlide].src}
+      alt={slides[currentSlide].alt}
+    />
   );
 }
 
